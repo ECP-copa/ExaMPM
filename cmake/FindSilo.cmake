15a96011dc3cb9428bf4f98984a9bbf476cde433
Stuart Slattery <slatterysr@ornl.gov>
2020-06-04 13:27:46 -0400

    fix warning in silo naming

--- cmake/FindSilo.cmake
+++ cmake/FindSilo.cmake
@@ -1,20 +1,20 @@
 find_package(PkgConfig QUIET)
-pkg_check_modules(PC_SILO silo QUIET)
+pkg_check_modules(PC_Silo silo QUIET)
 
-find_path(SILO_INCLUDE_DIR silo.h pmpio.h HINTS ${PC_SILO_INCLUDE_DIRS})
-find_library(SILO_LIBRARY NAMES silo siloh5 HINTS ${PC_SILO_LIBRARY_DIRS})
+find_path(Silo_INCLUDE_DIR silo.h pmpio.h HINTS ${PC_Silo_INCLUDE_DIRS})
+find_library(Silo_LIBRARY NAMES silo siloh5 HINTS ${PC_Silo_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SILO DEFAULT_MSG SILO_INCLUDE_DIR SILO_LIBRARY)
+find_package_handle_standard_args(Silo DEFAULT_MSG Silo_INCLUDE_DIR Silo_LIBRARY)
 
-mark_as_advanced(SILO_INCLUDE_DIR SILO_LIBRARY)
+mark_as_advanced(Silo_INCLUDE_DIR Silo_LIBRARY)
 
-if(SILO_INCLUDE_DIR AND SILO_LIBRARY)
+if(Silo_INCLUDE_DIR AND Silo_LIBRARY)
   add_library(Silo::silo UNKNOWN IMPORTED)
   set_target_properties(Silo::silo PROPERTIES
-    IMPORTED_LOCATION ${SILO_LIBRARY}
-    INTERFACE_INCLUDE_DIRECTORIES ${SILO_INCLUDE_DIR})
-  if(SILO_LINK_FLAGS)
-    set_property(TARGET Silo::silo APPEND_STRING PROPERTY INTERFACE_LINK_LIBRARIES " ${SILO_LINK_FLAGS}")
+    IMPORTED_LOCATION ${Silo_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES ${Silo_INCLUDE_DIR})
+  if(Silo_LINK_FLAGS)
+    set_property(TARGET Silo::silo APPEND_STRING PROPERTY INTERFACE_LINK_LIBRARIES " ${Silo_LINK_FLAGS}")
   endif()
 endif()
