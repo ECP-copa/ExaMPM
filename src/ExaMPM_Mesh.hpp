d53e661093da8fe2354c6f681085c644942bb909
Stephan Schulz <stephan.schulz-x2q@ruhr-uni-bochum.de>
2021-07-26 17:54:15 +0200

    remove unneeded methods

--- src/ExaMPM_Mesh.hpp
+++ src/ExaMPM_Mesh.hpp
@@ -40,17 +40,10 @@ class Mesh
     Mesh( const Kokkos::Array<double,6>& global_bounding_box,
           const std::array<int,3>& global_num_cell,
           const std::array<bool,3>& periodic,
-          const Cajita::ManualBlockPartitioner<3>& partitioner, //todo(sschulz): should work with Cajita::BlockPartitioner<3>!
+          const Cajita::BlockPartitioner<3>& partitioner,
           const int halo_cell_width,
           const int minimum_halo_cell_width,
           MPI_Comm comm )
-    : _global_bounding_box( global_bounding_box )
-    , _global_num_cell( global_num_cell )
-    , _periodic( periodic )
-    , _partitioner( partitioner )
-    , _halo_cell_width( halo_cell_width )
-    , _minimum_halo_cell_width( minimum_halo_cell_width )
-    , _comm( comm )
     {
         // Make a copy of the global number of cells so we can modify it.
         std::array<int,3> num_cell = global_num_cell;
@@ -106,51 +99,13 @@ class Mesh
             global_low_corner, global_high_corner, num_cell );
 
         // Build the global grid.
-        _global_grid = Cajita::createGlobalGrid(
+        auto global_grid = Cajita::createGlobalGrid(
             comm, global_mesh, periodic, partitioner );
 
         // Build the local grid.
         int halo_width = std::max( minimum_halo_cell_width, halo_cell_width );
-        _local_grid = Cajita::createLocalGrid( _global_grid, halo_width );
+        _local_grid = Cajita::createLocalGrid( global_grid, halo_width );
     }
-
-    void updateGeometry(
-            std::array<int,3>& local_offset,
-            std::array<int,3>& local_num_cell )
-    {
-        // Create global mesh bounds.
-        std::array<double,3> global_low_corner = { _global_bounding_box[0],
-                                                   _global_bounding_box[1],
-                                                   _global_bounding_box[2] };
-        std::array<double,3> global_high_corner = { _global_bounding_box[3],
-                                                    _global_bounding_box[4],
-                                                    _global_bounding_box[5] };
-        // For dimensions that are not periodic we pad by the minimum halo
-        // cell width to allow for projections outside of the domain.
-        double cell_size = _local_grid->globalGrid().globalMesh().cellSize(0);
-        for ( int d = 0; d < 3; ++d )
-        {
-            if ( !_periodic[d] )
-            {
-                global_low_corner[d] -= cell_size*_minimum_halo_cell_width;
-                global_high_corner[d] += cell_size*_minimum_halo_cell_width;
-            }
-        }
-
-        // Create the global mesh.
-        auto global_mesh = Cajita::createUniformGlobalMesh(
-            global_low_corner, global_high_corner, _global_num_cell );
-
-        // Build the global grid.
-        _global_grid = Cajita::createGlobalGrid(
-            _comm, global_mesh, _periodic, _partitioner, local_offset, local_num_cell );
-
-        // Build the local grid.
-        int halo_width = std::max( _minimum_halo_cell_width, _halo_cell_width );
-        _local_grid = Cajita::createLocalGrid( _global_grid, halo_width );
-        // todo(sschulz): Can we just reassing without causing a memory leak?
-    }
-
     
     // Get the local grid.
     const std::shared_ptr<Cajita::LocalGrid<Cajita::UniformMesh<double>>>& localGrid() const
@@ -184,18 +139,8 @@ class Mesh
 
   public:
 
-    Kokkos::Array<double,6> _global_bounding_box;
-    std::array<int,3> _global_num_cell;
-    std::array<bool,3> _periodic;
-    Cajita::ManualBlockPartitioner<3> _partitioner;
-    int _halo_cell_width;
-    int _minimum_halo_cell_width;
-    MPI_Comm _comm;
-
     std::shared_ptr<
       Cajita::LocalGrid<Cajita::UniformMesh<double>>> _local_grid;
-    std::shared_ptr<
-      Cajita::GlobalGrid<Cajita::UniformMesh<double>>> _global_grid;
 
     Kokkos::Array<int,3> _min_domain_global_node_index;
     Kokkos::Array<int,3> _max_domain_global_node_index;
